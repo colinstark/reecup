a6184d6104bbac110fda42b958ca78965bbeca76
colinstark <starkinbox@gmail.com>
2025-06-11 20:16:42 +0200

    added more handlers

--- server/handlers.go
+++ server/handlers.go
@@ -4,28 +4,92 @@ import (
 	"encoding/json"
 	"log"
 	"reecup/game"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-func (s *GameServer) handleLogin(data map[string]interface{}) {
-	// to implement. takes a userID and either creates a new user or retrieves the one stored in localstorage
+func (s *GameServer) handleUpdateName(data map[string]any, userID userID) {
+	name, ok := data["name"].(string)
+	if !ok {
+		s.handleError("login", "Invalid name", userID)
+		return
+	}
+
+	s.userMutex.Lock()
+
+	user := s.Users[userID]
+	user.Name = name
+
+	s.Users[userID] = user
+
+	s.userMutex.Unlock()
+
+	// Send success response
+	s.Send(userID, "name_updated", map[string]any{
+		"success": true,
+		"userID":  userID,
+		"name":    name,
+	})
 }
 
-func (s *GameServer) handleGetDeck(data map[string]interface{}, userID string) {
-	deck := game.CreateDeck()
+func (s *GameServer) handleLogin(data map[string]any, userID userID) {
+	name, ok := data["name"].(string)
+	if !ok {
+		s.handleError("login", "Invalid name", userID)
+		return
+	}
 
-	responseJSON, _ := json.Marshal(map[string]interface{}{
+	s.userMutex.Lock()
+	s.Users[userID] = User{
+		Name: name,
+	}
+	s.userMutex.Unlock()
+
+	// Send success response
+	s.Send(userID, "login", map[string]any{
+		"success": true,
+		"userID":  userID,
+	})
+}
+
+func (s *GameServer) handleLogout(data map[string]any, userID userID) {
+	s.userMutex.Lock()
+	delete(s.Connections, userID)
+	s.userMutex.Unlock()
+
+	// Notify all other users that this user has logged out
+	broadcastData := map[string]any{
+		"instruction": "user_logout",
+		"userID":      userID,
+	}
+	responseJSON, _ := json.Marshal(broadcastData)
+
+	for id, conn := range s.Connections {
+		if id != userID { // Don't send to the user who's logging out
+			conn.WriteMessage(websocket.TextMessage, responseJSON)
+		}
+	}
+
+	// Send confirmation to the logging out user
+	s.handleError("logout", "success", userID)
+}
+
+func (s *GameServer) handleGetDeck(data map[string]any, userID string) {
+	deck := game.CreateDeck()                   // Creates a new deck with stones
+	hand := game.DrawForNewPlayer(userID, deck) // Draw initial hand for player
+
+	responseJSON, _ := json.Marshal(map[string]any{
 		"instruction": "new_deck",
-		"deck":        deck,
+		"hand":        hand,
+		"remaining":   deck.GetCount(),
 	})
 
-	for _, conn := range s.clients {
-		conn.WriteMessage(websocket.TextMessage, responseJSON)
-	}
+	s.Connections[userID].WriteMessage(websocket.TextMessage, responseJSON)
 }
 
-func (s *GameServer) handleUpdateCursor(data map[string]interface{}, userID string) {
+func (s *GameServer) handleUpdateCursor(data map[string]any, userID string) {
 	name, ok := data["name"].(string)
 	if !ok {
 		return
@@ -42,7 +106,7 @@ func (s *GameServer) handleUpdateCursor(data map[string]interface{}, userID stri
 	}
 
 	// Notify all users in the channel
-	cursorJSON, _ := json.Marshal(map[string]interface{}{
+	cursorJSON, _ := json.Marshal(map[string]any{
 		"type": "update_cursor",
 		"from": userID,
 		"name": name,
@@ -51,16 +115,141 @@ func (s *GameServer) handleUpdateCursor(data map[string]interface{}, userID stri
 	})
 
 	// send to all
-	for str, conn := range s.clients {
+	for str, conn := range s.Connections {
 		log.Println("sending to ", cursorJSON, str)
 		conn.WriteMessage(websocket.TextMessage, cursorJSON)
 	}
 
-	responseJSON, _ := json.Marshal(map[string]interface{}{
+	responseJSON, _ := json.Marshal(map[string]any{
 		"type":    "success",
 		"message": "sent cursor",
 	})
 
 	// send to sender
-	s.clients[userID].WriteMessage(websocket.TextMessage, responseJSON)
+	s.Connections[userID].WriteMessage(websocket.TextMessage, responseJSON)
+}
+
+func (s *GameServer) handleNewGame(userID string) {
+	gameID := generateGameID()
+	newGame := game.NewGame()
+
+	s.gameMutex.Lock()
+	s.Games[gameID] = newGame
+	s.gameMutex.Unlock()
+
+	// Notify all users about new game
+	broadcastData := map[string]any{
+		"instruction": "game_created",
+		"game":        newGame,
+	}
+	responseJSON, _ := json.Marshal(broadcastData)
+	for id, _ := range s.Users {
+		s.Connections[id].WriteMessage(websocket.TextMessage, responseJSON)
+	}
+}
+
+func (s *GameServer) handleListGames(data map[string]any, userID string) {
+	s.gameMutex.RLock()
+	gamesList := make([]game.Game, 0, len(s.Games))
+	for _, g := range s.Games {
+		if g.State == "waiting" { // only list games that are waiting for players
+			gamesList = append(gamesList, g)
+		}
+	}
+	s.gameMutex.RUnlock()
+
+	responseJSON, _ := json.Marshal(map[string]any{
+		"instruction": "games_list",
+		"games":       gamesList,
+	})
+	s.Connections[userID].WriteMessage(websocket.TextMessage, responseJSON)
+}
+
+func (s *GameServer) handleJoinGame(data map[string]any, userID string) {
+	gameID, ok := data["gameID"].(string)
+	if !ok {
+		s.handleError("join_game", "invalid game ID", userID)
+		return
+	}
+
+	s.gameMutex.Lock()
+	game, exists := s.Games[gameID]
+	if !exists {
+		s.gameMutex.Unlock()
+		s.handleError("join_game", "game not found", userID)
+		return
+	}
+
+	// Check if player is already in game
+	for _, player := range game.Players {
+		if player.Name == userID {
+			s.gameMutex.Unlock()
+			s.handleError("join_game", "already in game", userID)
+			return
+		}
+	}
+
+	// Add player to game
+	// game.Players = append(game.Players, s.Users[userID]) @TODO fix this
+	s.Games[gameID] = game
+	s.gameMutex.Unlock()
+
+	// Notify all users about player joining
+	broadcastData := map[string]any{
+		"instruction": "player_joined",
+		"gameID":      gameID,
+		"playerID":    userID,
+		"game":        game,
+	}
+
+	s.Broadcast("banana", broadcastData)
+}
+
+func (s *GameServer) handleCancelGame(data map[string]any, userID string) {
+	gameID, ok := data["gameID"].(string)
+	if !ok {
+		s.handleError("cancel_game", "invalid game ID", userID)
+		return
+	}
+
+	s.gameMutex.Lock()
+	game, exists := s.Games[gameID]
+	if !exists {
+		s.gameMutex.Unlock()
+		s.handleError("cancel_game", "game not found", userID)
+		return
+	}
+
+	// Check if game is in progress - if so, return error
+	if game.State == "in_progress" {
+		s.gameMutex.Unlock()
+		s.handleError("cancel_game", "cannot cancel game in progress", userID)
+		return
+	}
+
+	// Remove the game from the games map
+	delete(s.Games, gameID)
+	s.gameMutex.Unlock()
+
+	// Notify all users about game cancellation
+	broadcastData := map[string]any{
+		"instruction": "game_cancelled",
+		"gameID":      gameID,
+	}
+
+	s.Broadcast("game_cancelled", broadcastData)
+
+	// Send success response to the user who cancelled
+	s.Send(userID, "cancel_game", map[string]any{
+		"success": true,
+		"gameID":  gameID,
+	})
+}
+
+func generateGameID() gameID {
+	return "game_" + strconv.FormatInt(time.Now().UnixNano(), 6)
+}
+
+func generateUserID() userID {
+	return "user_" + strconv.FormatInt(time.Now().UnixNano(), 6)
 }
--- server/server.go
+++ server/server.go
@@ -4,23 +4,26 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
 	"reecup/game"
 	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
+type userID = string
+type gameID = string
+
 type GameServer struct {
-	upgrader  websocket.Upgrader
-	clients   map[string]*websocket.Conn
-	users     map[string]User
-	games     map[string]game.Game
-	userMutex sync.RWMutex
-	gameMutex sync.RWMutex
+	upgrader    websocket.Upgrader
+	Connections map[userID]*websocket.Conn
+	Users       map[userID]User      `json:"users"`
+	Games       map[gameID]game.Game `json:"games"`
+	userMutex   sync.RWMutex
+	gameMutex   sync.RWMutex
 }
 
 type User struct {
-	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
@@ -31,9 +34,9 @@ func NewGameServer() *GameServer {
 				return true
 			},
 		},
-		clients: make(map[string]*websocket.Conn),
-		users:   make(map[string]User),
-		games:   make(map[string]game.Game),
+		Connections: make(map[userID]*websocket.Conn),
+		Users:       make(map[userID]User),
+		Games:       make(map[gameID]game.Game),
 	}
 }
 
@@ -45,23 +48,32 @@ func (s *GameServer) HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
+	log.Println("NEW CONNECTION")
+
 	userID := getUserIDFromRequest(r)
+
 	if userID == "" {
 		log.Println("No user ID provided")
-		return
+		newUserID := generateUserID()
+		log.Println("Sending ID", newUserID)
+		conn.WriteJSON(map[string]any{
+			"type":    "user_id_commissioned",
+			"user_id": newUserID,
+		})
+	} else {
+		s.registerClient(userID, conn)
 	}
-
-	s.registerClient(userID, conn)
 
 	// Main loop
 	for {
 		_, message, err := conn.ReadMessage()
+		log.Println("GOT MESSAGE")
 		if err != nil {
 			log.Println("Read error:", err)
 			break
 		}
 
-		var data map[string]interface{}
+		var data map[string]any
 		if err := json.Unmarshal(message, &data); err != nil {
 			log.Println("JSON error:", err)
 			continue
@@ -69,21 +81,27 @@ func (s *GameServer) HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 
 		switch data["instruction"] {
 		case "login":
-		// s.handleLogin(data, userID)
+			s.handleLogin(data, userID)
+		case "logout":
+			s.handleLogout(data, userID)
+		case "update_name":
+			s.handleUpdateName(data, userID)
 		case "new_game":
-		// s.handleNewGame(data, userID)
+			s.handleNewGame(userID)
 		case "list_games":
-		// s.handleListGames(data, userID)
+			s.handleListGames(data, userID)
+		case "cancel_game":
+			s.handleCancelGame(data, userID)
 		case "join_game":
-		// s.handleJoinGame(data, userID)
+			s.handleJoinGame(data, userID)
 		case "draw_stone":
-		// s.handleDrawStone(data, userID)
+			// s.handleDrawStone(data, userID)
 		case "finish_turn":
 			// s.handleFinishTurn(data, userID)
-		case "update_cursor":
-			s.handleUpdateCursor(data, userID)
 		case "get_deck":
 			s.handleGetDeck(data, userID)
+		case "update_cursor":
+			// s.handleUpdateCursor(data, userID)
 		default:
 			s.handleError("general", "Invalid Event", userID)
 		}
@@ -94,48 +112,82 @@ func (s *GameServer) HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 
 func (s *GameServer) registerClient(userID string, conn *websocket.Conn) {
 	s.userMutex.Lock()
-	s.clients[userID] = conn
-	s.userMutex.Unlock()
+	defer s.userMutex.Unlock()
+
+	s.Connections[userID] = conn
+
+	if _, exists := s.Users[userID]; !exists {
+		s.Users[userID] = User{
+			Name: "", // will be set later async
+		}
+	}
 }
 
 func (s *GameServer) unregisterClient(userID string, conn *websocket.Conn) {
 	_ = conn
 	s.userMutex.Lock()
-	delete(s.clients, userID)
-	s.userMutex.Unlock()
+	defer s.userMutex.Unlock()
+
+	delete(s.Connections, userID)
 }
 
-func (s *GameServer) handleError(instruction string, error string, senderID string) {
-	responseJSON, _ := json.Marshal(map[string]interface{}{
-		"instruction": instruction,
-		"error":       error,
-	})
-
-	s.clients[senderID].WriteMessage(websocket.TextMessage, responseJSON)
-}
-
-func (s *GameServer) Broadcast(instruction string, data map[string]interface{}) {
-	responseJSON, _ := json.Marshal(data)
-
-	for _, conn := range s.clients {
-		conn.WriteMessage(websocket.TextMessage, responseJSON)
+func (s *GameServer) handleError(instruction string, error string, targetID string) {
+	conn := s.Connections[targetID]
+	if conn != nil {
+		conn.WriteJSON(map[string]any{
+			"instruction": instruction,
+			"error":       error,
+		})
 	}
 }
 
-func (s *GameServer) NotifyPlayersOfGame(gameID string, instruction string, data map[string]interface{}) {
-	// @TODO: Make this work
-	// responseJSON, _ := json.Marshal(data)
+func (s *GameServer) Send(target string, instruction string, data map[string]any) {
+	conn := s.Connections[target]
+	if conn != nil {
+		json, err := marshal(instruction, data)
+		if err == nil {
+			conn.WriteMessage(websocket.TextMessage, json)
+		}
+	}
+}
 
-	// for _, conn := range Game.players {
-	// 	conn.WriteMessage(websocket.TextMessage, responseJSON)
+func (s *GameServer) Broadcast(instruction string, data map[string]any) {
+	json, err := marshal(instruction, data)
+	if err != nil {
+		return
+	}
+
+	for _, conn := range s.Connections {
+		conn.WriteMessage(websocket.TextMessage, json)
+	}
+}
+
+func (s *GameServer) BroadcastInGame(gameID string, instruction string, data map[string]interface{}) {
+	// @TODO: Make this work
+	//
+	// json, _ := marshal(instruction, data)
+
+	// for _, user := range Game.Players {
+	// 	user.Conn.WriteMessage(websocket.TextMessage, json)
 	// }
 }
 
-func (s *GameServer) SendMessage(targetID string, instruction string, data map[string]interface{}) {
-	responseJSON, _ := json.Marshal(data)
-	s.clients[targetID].WriteMessage(websocket.TextMessage, responseJSON)
+func marshal(instruction string, data map[string]any) ([]byte, error) {
+	data["instruction"] = instruction
+	responseJSON, err := json.Marshal(data)
+	if err != nil {
+		log.Println("Marshal error:", err)
+		return []byte{}, err
+	}
+
+	return responseJSON, nil
 }
 
 func getUserIDFromRequest(r *http.Request) string {
-	return r.URL.Query().Get("userID")
+	userID := r.URL.Query().Get("userID")
+	if userID == "" {
+		log.Println("No user ID provided")
+	}
+
+	return userID
 }
